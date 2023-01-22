0295bde23007c4f204ae2a62ee49d602f4914e8c
Aguinele Queiroz <aguinelequeiroz@outlook.com>
2023-01-22 12:12:56 -0300

    new feature: refresh list after add new transaction

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,10 +42,18 @@ function processDataInputs(natureOfTransaction, typeTransaction, descriptionTran
     }
 }
 
+/* refresh list of transactions */
+const refreshListTransactions = () => {
+    const transactionsList = document.querySelector(".transactions-list");
+    transactionsList.innerText = '';
+    showTransactionsInDOM();
+}
+
 /* call of the functions for the add new transacts */
 const addNewTransaction = (event) => {
     event.preventDefault();
     getDataForm();
+    refreshListTransactions();
 }
 
 /* events on html ================================================================================== */
@@ -73,9 +81,18 @@ function hideMessageErrorInDOM() {
 }
 
 /* verify value of Nature of transaction */
-const verifyNatureTransaction = (obj) => {
-    const natureTransaction = obj["natureOfTransaction"] === "pay" ? "minus" : "add";
-    return natureTransaction;
+const natureTransactionCssClass = (obj) => obj["natureOfTransaction"] === "pay" ? "minus" : "add";
+
+/* add "+" or "-" before each transaction amount */
+function addTransactionMeaning() {
+    const meaning = document.querySelector(".meaning-transaction");
+    const tag = document.querySelector(".tag-nature-transaction");
+    const add = document.createTextNode('+');
+    const minus = document.createTextNode('-');
+    if (tag) {
+        tag.classList.contains("minus") ? meaning.appendChild(minus) : meaning.appendChild(add);
+        meaning.textContent === "-" ? meaning.classList.add("red") : meaning.classList.add("green");
+    }
 }
 
 function renderTransactionsInDOM(transaction, CSSClass) {
@@ -87,25 +104,51 @@ function renderTransactionsInDOM(transaction, CSSClass) {
                 <p class="description-transaction">${transaction.descriptionTransaction}</p>
             </div>
             <div class="value-transact">
-              <span>+ R$</span>
+              <span class="meaning-transaction"></span>
               <span>${transaction.valueTransaction}</span>
             </div>
             <div class="tag-nature-transaction"><div>`;
     transactionHtml.classList.add("card-transaction");
     transactionHtml.children[2].classList.add(CSSClass);
-
     transactionsList.prepend(transactionHtml);
 }
+
+// const isNumeric = (value) => {
+
+// }
+
+// function updateInfoFinanceValues(obj, natureTransaction) {
+//     const amount = document.getElementById("amount");
+//     if (natureTransaction === "minus") {
+//         const valueTransacion = Number(obj["valueTransaction"]);
+
+//     }
+// }
+
+// function updateAmount() {
+
+// }
+
+// function updateIncomes() {
+
+// }
+
+// function updateExpenses() {
+
+// }
 
 const showTransactionsInDOM = () => {
     if (getTransactionsOnLocalStorage) {
         const arrAllTransactions = JSON.parse(localStorage.getItem(keyOfDataLocalStorage));
-        arrAllTransactions.forEach(obj => renderTransactionsInDOM(obj, verifyNatureTransaction(obj)));
+        arrAllTransactions.forEach(obj => {
+            renderTransactionsInDOM(obj, natureTransactionCssClass(obj));
+            addTransactionMeaning();
+
+        });
     }
     return;
 }
 
 showTransactionsInDOM();
-
 
 btnRegisterTransaction.addEventListener('click', addNewTransaction)
